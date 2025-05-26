8dd8b976904f491a1ab65d0032bf6ba093b25045
HagenK42 <666.thurisaz.666@gmail.com>
2025-05-26 16:17:12 +0200

    working adminpage to add, update and delete products

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.getProducts),
+    path('admin/', admin.site.urls),
     path('add/', views.addProduct),
+    path('update/', views.updateProduct),
+    path('delete/', views.deleteProduct),
 ]
--- products/views.py
+++ products/views.py
@@ -18,3 +18,22 @@ def addProduct(request):
         serializer.save()
         return Response(serializer.data, status=201)
     return Response(serializer.errors, status=400)
+
+@api_view(['POST'])
+def updateProduct(request, pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(instance=product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
+
+@api_view(['POST'])
+def deleteProduct(request):
+    product_id = request.data.get('id')
+    try:
+        product = Product.objects.get(id=product_id)
+        product.delete()
+        return Response({'message': 'Product deleted successfully'}, status=204)
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=404)
