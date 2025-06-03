ae24277d29a672412b790d2eaa97ba7770b98aaa
HagenK42 <666.thurisaz.666@gmail.com>
2025-06-03 22:07:11 +0200

    debug = False

--- ecomm_backend/settings.py
+++ ecomm_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jz=cj$1l%k4a%^1&2szxuc54wqqxug0xqpu=(pckc)v4gr8hdq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
